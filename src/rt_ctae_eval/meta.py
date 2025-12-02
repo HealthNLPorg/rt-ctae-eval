5e2b60f8176b51586e5811376ee2c172b3aee937
Eli Goldner <etgld@posteo.us>
2025-12-02 16:04:38 -0500

    Forgot to add meta

---
+++ src/rt_ctae_eval/meta.py
@@ -0,0 +1,10 @@
+
+from enum import Enum
+
+RT_CUI = "C1522449"
+
+class NaranjoScale(Enum):
+    DOUBTFUL = "Doubtful"
+    POSSIBLE = "Possible"
+    PROBABLE = "Probable"
+    CERTAIN = "Certain"
